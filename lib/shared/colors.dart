c49228584cd81306dab80b01269d7013cae9665e
Erica Rodrigues <ericamrodrigs@gmail.com>
2023-12-02 17:30:28 -0300

    feat: :art: customize design styles

---
+++ lib/shared/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Colors.blueGrey;
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color black = Color(0xFF000000);
+  static const Color error = Color(0xFFEF4F2B);
+}
---
+++ lib/shared/text_styles.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppTextStyles {
+  static TextStyle bold14BlueGrey700() {
+    return TextStyle(
+      fontSize: 14,
+      fontWeight: FontWeight.bold,
+      color: Colors.blueGrey[700],
+    );
+  }
+}
